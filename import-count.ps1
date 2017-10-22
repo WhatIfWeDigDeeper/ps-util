f0f5511c09032e6550d78cd4641be6ec4a98896c
WhatIfWeDigDeeper <whatifwedigdeeper@gmail.com>
2017-10-22 09:04:57 -0500

    parameterized script

--- import-count.ps1
+++ import-count.ps1
@@ -1,2 +1,9 @@
+Param(
+  [string]$library = 'lodash',
+  [string]$srcPath = '.',
+  [string]$fileFilter = '*.js'
+)
+
 $lookup = @{}
-gci -Filter *.js -Recurse | Get-Content | Where-Object {$_ -imatch "import (\w+) from 'lodash/(\w+)'.*" } | ForEach-Object { write-host $matches[1] }
+gci $srcPath -Filter $fileFilter -Recurse | Get-Content | Where-Object {$_ -imatch "import (\w+) from '$library/(\w+)'.*" } | ForEach-Object { if ($lookup.containsKey($matches[1])) { $lookup.set_item($matches[1], $lookup.get_item($matches[1]) + 1) } else { $lookup.add($matches[1], 1)} }
+$lookup.getEnumerator() | Sort-Object -Property @{Expression = "value"; Descending = $True}, @{Expression = "name"; Descending = $False}
